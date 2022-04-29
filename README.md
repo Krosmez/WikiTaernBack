c8f638b79d2912e0ef8e1c91b17d5648c4481425
Jo M <jonathanmaison78@gmail.com>
2022-04-29 10:07:26 +0200

    [ADD / UPD] Make the default startApp qith DB login. README had been updated to explain the variables

---
+++ README.md
@@ -0,0 +1,11 @@
+# env
+
+For the moment you must have a .env file or folder which should contains those parameter :
+
+- DB_USER = 'your_username'
+- DB_PWD = 'your_secret_password'
+- DB_NAME = 'the_database_name'
+- CLUSTER_NAME = 'the_cluster_name'
+- PORT = the_port_you_re_working_on
+
+Noticed that every default parameter in this app are FAKE, they do not provide access to any database nor data.
---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const local = "dotenv/config";
+require(local);
+
+const startApplication = async () => {
+    await mongoose.connect(
+        `mongodb+srv://${process.env.DB_USER || 'username'}:${process.env.DB_PWD || 'secured_pwd'}@${process.env.CLUSTER_NAME || 'Cluster0'}.3vm4z.mongodb.net/${process.env.DB_NAME || 'myFirstDatabase'}?retryWrites=true&w=majority`,
+        {
+            useUnifiedTopology: true,
+            useNewURLParser: true
+        }
+    );
+    console.log(`Successfully connected to MongoDB at: ${process.env.DB_NAME || "local"} on ${process.env.CLUSTER_NAME || "noCluster"}`);
+
+    await app.listen(process.env.PORT || 8080);
+    console.log(`Listening on ${process.env.PORT || 8080}`);
+}
+
+const app = express();
+app.use(express.json());
+
+startApplication();
