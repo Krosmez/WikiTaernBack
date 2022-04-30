6b554bce9fbc331c2b07671b937a563b39c7fc98
Jo M <jonathanmaison78@gmail.com>
2022-04-30 15:49:52 +0200

    [ADD] Make the file for routes GET and POST

---
+++ routes/itemsRoute.js
@@ -0,0 +1,83 @@
+const express = require("express");
+const items = require("../models/itemsModel");
+
+const itemsRouter = express.Router();
+
+// List all items
+itemsRouter.get('/all', async (_, res) => {
+    items.find({}).then(
+        (data) => {
+            console.log(data)
+            res.status(200).json(data);
+        }
+    ).catch(
+        (err) => {
+            console.log(err)
+            res.status(400).json({ message: err });
+        }
+    )
+})
+
+// Create a new item as Stuff
+itemsRouter.post('/stuff', async (req, res) => {
+    const { name, value, weight, rarity, isFor, stats, effects, isPartOfSet } = req.body;
+    // create a new item
+    const newItem = new items({
+        "isStuff": {
+            name,
+            value,
+            weight,
+            rarity,
+            isFor,
+            stats,
+            effects,
+            isPartOfSet
+        },
+        "isMaterial":false
+    });
+
+    newItem.save((err, result) => {
+        if (err) {
+            res.status(500).send(
+                {
+                    message: err
+                }
+            )
+            return;
+        }
+        res.status(200).json({ result })
+    })
+})
+
+// Create a new item as Material
+itemsRouter.post('/material', async (req, res) => {
+    const { name, value, weight, position, utility, isNatural, isFromPNJ} = req.body;
+    // create a new item
+    const newItem = new items({
+        "isStuff":false,
+        "isMaterial": {
+            name,
+            value,
+            weight,
+            position,
+            utility,
+            isNatural,
+            isFromPNJ
+        }
+    });
+
+    newItem.save((err, result) => {
+        if (err) {
+            res.status(500).send(
+                {
+                    message: err
+                }
+            )
+            return;
+        }
+        console.log(result)
+        res.status(200).json({ result })
+    })
+})
+
+module.exports = itemsRouter;
