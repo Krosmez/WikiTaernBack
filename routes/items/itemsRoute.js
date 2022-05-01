9bbe5cb547208e1179917c7eca74f50be45853f3
Jo M <jonathanmaison78@gmail.com>
2022-05-01 12:27:28 +0200

    [ADD] route for stuff/all

--- routes/items/itemsRoute.js
+++ routes/items/itemsRoute.js
@@ -3,6 +3,29 @@ const items = require("../../models/itemsModel");
 
 const itemsRouter = express.Router();
 
+itemsRouter.get('/stuff/all', async (req, res) => {
+    items.find({ isStuff: {} })
+        .exec()
+        .then(
+            (data) => {
+                res.status(200).json(data);
+            }
+        ).catch(
+            (err) => {
+                res.status(500).json({ message: err })
+            }
+        )
+
+
+})
+
+// itemsRouter.get('/stuff/:id', async (req, res) => {
+//     const { id } = req.params;
+
+
+// })
+
+
 // Create a new item as Stuff
 itemsRouter.post('/stuff', async (req, res) => {
     const { name, value, weight, rarity, isFor, stats, effects, isPartOfSet } = req.body;
@@ -53,7 +76,7 @@ itemsRouter.put('/stuff/:id', async (req, res) => {
         res.status(400).json({
             errors: errors.array()
         });
-        return ;
+        return;
     };
 
     await items.findOneAndUpdate({ _id: id }, body, (error, res) => {
