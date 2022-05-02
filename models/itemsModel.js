0600c1788535cacb5df715507ad4cdf11c101f8e
Jo M <jonathanmaison78@gmail.com>
2022-05-02 11:53:58 +0200

    [ADD] items model for rework

---
+++ models/itemsModel.js
@@ -0,0 +1,63 @@
+const mongoose = require("mongoose");
+
+const items = mongoose.model(
+    "Items",
+    new mongoose.Schema(
+        {
+            "name": String,
+            "value": Number,
+            "weight": Number,
+            "stuff": {
+                "rarity": String,
+                "is_for": String,
+                "stats": [{ "name": String, "value": Number }],
+                "effects": [{ "name": String, "value": Number }],
+                'part_of_set': {
+                    "name": String,
+                    "effects": [{ "name": String, "value": Number, "description": String }],
+                    "with": [{ "name": String }]
+                }
+            },
+            "material": {
+                "position": String,
+                "utility": String,
+                "natural": {
+                    "extract_from": {
+                        "position": String,
+                        "qty": Number
+                    },
+                    "process_from": {
+                        "position": String,
+                        "qty": Number
+                    },
+                    "from_mob": {
+                        "mob_id": { type: mongoose.Schema.Types.ObjectId, ref: 'characters' },
+                        "name": String
+                    }
+                },
+                "from_pnj": {
+                    "pnjId": { type: mongoose.Schema.Types.ObjectId, ref: 'characters' },
+                    "buy_from": [
+                        {
+                            "pnj_id": { type: mongoose.Schema.Types.ObjectId, ref: 'characters' },
+                            "trade_value": Number,
+                            "currency": String,
+                            "qty": Number
+                        }
+                    ],
+                    "by_trade": {
+                        "obtain_with": [
+                            {
+                                "pnjId": { type: mongoose.Schema.Types.ObjectId, ref: 'characters' },
+                                "name": String
+                            }
+                        ]
+                    },
+                    "tasks": [{ "qty": Number }]
+                }
+            }
+        }
+    )
+);
+
+module.exports = items;
