0f8bfa43c169a2e880690a337821e33998c0253e
Jo M <jonathanmaison78@gmail.com>
2022-05-02 08:35:43 +0200

    [ADD] GET route for charcters

--- routes/characters/charactersRoute.js
+++ routes/characters/charactersRoute.js
@@ -3,3 +3,14 @@ const characters = require('../../models/charactersModel');
 
 const charactersRouter = express.Router();
 
+charactersRouter.get('/all', async (_, res) => {
+    characters.find({}).then(
+        (data) => {
+            res.status(200).json(data);
+        }
+    ).catch(
+        (err) => {
+            res.status(500).json({ message: err });
+        }
+    );
+})
