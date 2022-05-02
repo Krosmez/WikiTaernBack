21828288169ba7963474392413a6bbc8ebcb302a
Jo M <jonathanmaison78@gmail.com>
2022-05-02 11:35:45 +0200

    [ADD] get query by filter

--- routes/characters/charactersRoute.js
+++ routes/characters/charactersRoute.js
@@ -15,4 +15,31 @@ charactersRouter.get('/all', async (_, res) => {
     );
 })
 
+charactersRouter.get('/pnj', async (_, res) => {
+    characters.find({
+        $and: [
+            {
+                "isMob.isNormal": {
+                    $eq: "false"
+                }
+            },
+            {
+                "isMob.isBoss": {
+                    $eq: "false"
+                }
+
+            },
+            {
+                "isMob.isChampion": {
+                    $eq: "false"
+                }
+            }
+        ]
+    },
+        'id name position isPNJ'
+    )
+        .then((data) => { res.status(200).json(data) })
+        .catch((err) => { res.status(500).json({ message: err }) })
+})
+
 module.exports = charactersRouter;
