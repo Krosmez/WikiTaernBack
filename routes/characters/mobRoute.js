6c68ef8a928aeaf8d758d13acd47f8c92ade6cfe
Jo M <jonathanmaison78@gmail.com>
2022-05-04 09:40:50 +0200

    [ADD/UPD] get mob by id - Change returned field

--- routes/characters/mobRoute.js
+++ routes/characters/mobRoute.js
@@ -6,9 +6,27 @@ const mobRouter = express.Router();
 mobRouter.get('/', async (_, res) => {
     characters.find({
         is_mob: { $eq: true }
-    }, 'id name position pnj')
+    }, 'id name position mob')
         .then((data) => { res.status(200).json(data) })
         .catch((err) => { res.status(500).json({ message: err }) });
+});
+
+mobRouter.get('/:id', async (req, res) => {
+    const { id } = req.params;
+    await characters.find({
+        $and: [
+            { id },
+            {
+                is_mob: {
+                    $eq: true
+                }
+            }
+        ]
+    }, 'id name position mob').then(
+        (data) => {
+            res.status(200).json(data)
+        }
+    ).catch((err) => { res.status(500).json({ message: err }) })
 });
 
 module.exports = mobRouter;
