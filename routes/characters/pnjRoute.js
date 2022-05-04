0628e4cd693dcf1eb21d09aedf88c38bb9039c79
Jo M <jonathanmaison78@gmail.com>
2022-05-04 09:42:20 +0200

    [ADD] get pnj by id

--- routes/characters/pnjRoute.js
+++ routes/characters/pnjRoute.js
@@ -11,4 +11,21 @@ pnjRouter.get('/', async (_, res) => {
         .catch((err) => { res.status(500).json({ message: err }) })
 })
 
+pnjRouter.get('/:id', async (req, res) => {
+    const { id } = req.params;
+    await characters.find({
+        $and: [
+            { id },
+            {
+                is_material: {
+                    $eq: true
+                }
+            }
+        ]
+    }, 'id name position pnj').then(
+        (data) => {
+            res.status(200).json(data)
+        }
+    ).catch((err) => { res.status(500).json({ message: err }) })
+});
 module.exports = pnjRouter;
